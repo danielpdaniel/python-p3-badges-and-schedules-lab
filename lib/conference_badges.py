8dbcf2573a48ece3b6066fbc36eb141e48095098
danielpdaniel <artofjquinn@gmail.com>
2023-06-27 20:41:14 -0400

    Update functions to get tests passing using for loops

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    names_list = []
+    for name in names:
+        badge_text = badge_maker(name)
+        names_list.append(badge_text)
+    return names_list
+
 
 def assign_rooms(names):
-    return None
+    rooms_list = []
+    for n in range(1,9):
+        rooms_list.append(f"Hello, {names[n -1]}! You'll be assigned to room {n}!")
+    return rooms_list
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
+    
