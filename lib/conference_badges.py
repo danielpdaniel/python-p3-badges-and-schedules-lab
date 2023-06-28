4a1443437aae9ffd27c436549136637e0b523c55
danielpdaniel <artofjquinn@gmail.com>
2023-06-27 20:45:42 -0400

    update batch_badge_creator and assign_rooms to use list comprehension rather than for loops

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -2,17 +2,17 @@ def badge_maker(name):
     return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    names_list = []
-    for name in names:
-        badge_text = badge_maker(name)
-        names_list.append(badge_text)
+    names_list = [badge_maker(name) for name in names]
+    # for name in names:
+    #     badge_text = badge_maker(name)
+    #     names_list.append(badge_text)
     return names_list
 
 
 def assign_rooms(names):
-    rooms_list = []
-    for n in range(1,9):
-        rooms_list.append(f"Hello, {names[n -1]}! You'll be assigned to room {n}!")
+    rooms_list = [f"Hello, {names[n -1]}! You'll be assigned to room {n}!" for n in range(1,9)]
+    # for n in range(1,9):
+    #     rooms_list.append(f"Hello, {names[n -1]}! You'll be assigned to room {n}!")
     return rooms_list
 
 
